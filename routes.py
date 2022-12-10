6c569f774933051b686419112bc8423df7b4c252
swijnands97 <scpwijnands@gmail.com>
2022-12-10 17:05:31 +0100

    Added file.

---
+++ routes.py
@@ -0,0 +1,36 @@
+# Import relevant modules
+from models import Parameters
+import numpy as np
+import pandas as pd
+
+# Initialize lists
+return_routes = []
+return_ranges = []
+tri_routes = []
+tri_ranges = []
+
+# Set parameters from Parameters class
+parameters = Parameters()
+
+# Get aircraft ranges and airport distance matrix
+aircraft_ranges = parameters.Range
+distances = parameters.distance_matrix
+airports = list(distances.index)
+
+# Compute return distances from hub
+for _ in distances['LIRA']:
+    return_ranges.append(_*2)
+
+for _ in airports:
+    return_routes.append(['LIRA', _, 'LIRA'])
+
+# Create DataFrame with return flights from hub
+bi_flights = pd.DataFrame(np.array(return_ranges), np.array(return_routes), columns=['range'])
+
+# Create DataFrame with triangular flights from hub
+
+#TBD
+
+
+
+
