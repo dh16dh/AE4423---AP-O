e226d704c7ebcee1cd1367f5631dcdaace14a215
davidahartong <davidahartong@gmail.com>
2023-01-16 10:27:55 +0100

    Changed csv files to use . instead of , so Python knows they're floats and not objects

--- Assignment 2/Parameters.py
+++ Assignment 2/Parameters.py
@@ -12,10 +12,32 @@ class Parameters:
                  recapture_rate='Group_data_17/Recapture_rate.csv'
                  ):
 
-        self.aircraft_data = pd.read_csv(aircraft_data, index_col=0)
-        self.flight_data = pd.read.csv(flight_data, index_col=0)
-        self.itinerary_data = pd.read_csv(itinerary_data, index_col=0)
-        self.new_flight_data = pd.read.csv(new_flight_data, index_col=0)
-        self.new_itinerary_data = pd.read.csv(new_itinerary_data, index_col=0)
-        self.optional_flight_data = pd.read.csv(optional_flight_data, index_col=0)
-        self.recapture_rate = pd.read.csv(recapture_rate)
+        self.aircraft_data = pd.read_csv(aircraft_data, sep=';')
+        self.flight_data = pd.read_csv(flight_data, sep=';')
+        self.itinerary_data = pd.read_csv(itinerary_data, sep=';')
+        self.new_flight_data = pd.read_csv(new_flight_data, sep=';')
+        self.new_itinerary_data = pd.read_csv(new_itinerary_data, sep=';')
+        self.optional_flight_data = pd.read_csv(optional_flight_data, sep=';')
+        self.recapture_rate = pd.read_csv(recapture_rate, sep=';')
+
+        self.N = self.flight_data['ORG'].unique()
+        self.K = self.aircraft_data['Type']
+        self.L = self.flight_data['Flight Number']
+        self.P = self.itinerary_data['Itin No.']
+
+        for k in self.K:
+            filtered_flights = self.flight_data[self.flight_data[k] < 1000000]
+            for n in self.N:
+                destination_set = self.flight_data[self.flight_data['DEST'] == n].drop(columns=['ORG', 'Departure'])
+                origin_set = self.flight_data[self.flight_data['ORG'] == n].drop(columns=['DEST', 'Arrival'])
+                destination_set.rename(columns={'DEST': 'IATA'}, inplace=True)
+                origin_set.rename(columns={'ORG': 'IATA'}, inplace=True)
+
+
+
+
+
+
+if __name__ == "__main__":
+    parameters = Parameters()
+
