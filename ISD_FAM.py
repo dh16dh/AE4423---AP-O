b8abae192ac727f08f395d7c786a68fbfacdfe8f
davidahartong <davidahartong@gmail.com>
2023-01-20 15:20:05 +0100

    Added ISD_FAM

--- ISD_FAM.py
+++ ISD_FAM.py
@@ -1,7 +1,5 @@
 # Import relevant modules
 import pandas as pd
-import numpy as np
-import plotly.graph_objects as go
 from gurobipy import Model, GRB, Column, LinExpr, quicksum
 from Parameters import Parameters
 import time
